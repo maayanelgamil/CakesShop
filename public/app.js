77cfdb545cce1f983af98fe70e932f0e20010882
Maayan Elgamil <maayan.elgamil@gmail.com>
2017-06-25 18:00:28 +0300

    add $root

--- public/app.js
+++ public/app.js
@@ -6,28 +6,32 @@ app.config(['$locationProvider', function($locationProvider) {
     $locationProvider.hashPrefix('');
 }]);
 
-app.controller('mainController', ['$scope', 'UserService', '$location', '$window', '$http','localStorageService', '$filter',
-    function($scope, UserService, $location, $window,  $http, localStorageService, $filter) {
+app.controller('mainController', ['$scope', 'UserService', '$location', '$window', '$http','localStorageService', '$filter', '$rootScope',
+    function($scope, UserService, $location, $window,  $http, localStorageService, $filter, $rootScope) {
     let self = this;
-    self.UserName = '';
-    self.LastLogin = '';
-    self.guest=true;
-        if(localStorageService.cookie.isSupported){
-            let user = localStorageService.cookie.get('user');
-            if(user){
-                self.UserName = user.UserName;
-                self.LastLogin = user.Date;
-
-
-                self.LastLogin = $filter('date')(self.LastLogin, "dd/MM/yyyy");
-                self.guest=false;
-            }
-        }
+    UserService.initUser($rootScope);
 }]);
 
 //-------------------------------------------------------------------------------------------------------------------
-app.factory('UserService', ['$http', function($http) {
+app.factory('UserService', ['$http', 'localStorageService', '$filter', function($http, localStorageService, $filter) {
     let service = {};
+
+    service.initUser = function($rootScope){
+        $rootScope.guest = true;
+        $rootScope.UserName = '';
+        $rootScope.LastLogin = '';
+        if(localStorageService.cookie.isSupported){
+            let user = localStorageService.cookie.get('user');
+            if(user){
+                $rootScope.UserName = user.UserName;
+                $rootScope.LastLogin = user.Date;
+
+
+                $rootScope.LastLogin = $filter('date')($rootScope.LastLogin, "dd/MM/yyyy");
+                $rootScope.guest=false;
+            }
+        }
+    };
 
     service.getNewProducts = function(){
         return $http.get('cakes//getNewCakes').then(function(response){
@@ -57,21 +61,15 @@ app.factory('UserService', ['$http', function($http) {
 app.config( ['$routeProvider', function($routeProvider) {
     $routeProvider
         .when("/", {
-            templateUrl : "views/home.html",
-            controller : "productsController"
-        })
+            templateUrl : "views/home.html"})
         .when("/login", {
-            templateUrl : "views/login.html",
-            controller : "loginController"
-        })
+            templateUrl : "views/login.html"})
         .when("/register", {
-            templateUrl : "views/register.html",
-            controller: "registerController"
-        }).when("/about", {
+            templateUrl : "views/register.html"
+        }).when("/cakes", {
+        templateUrl : "views/cakes.html"
+    }).when("/about", {
         templateUrl : "views/about.html"
-    }).when("/cakes", {
-        templateUrl : "views/cakes.html",
-        controller: "cakesController"
     }).otherwise({
         redirectTo : "/"
     });
--- public/homeController.js
+++ public/homeController.js
@@ -1,16 +1,18 @@
 
 'use strict';
 //-------------------------------------------------------------------------------------------------------------------
-app.controller('productsController', ['$scope', '$http','localStorageService', function($scope, $http, localStorageService) {
+app.controller('productsController', ['$scope', '$http','localStorageService', '$rootScope', function($scope, $http, localStorageService, $rootScope) {
     let self = this;
+    if(!$rootScope.top5){
+        $http.get('/cakes/top5')
+            .then(function (res) {
+                $rootScope.top5 = res.data;
+            })
+            .catch(function (e) {
+                return Promise.reject(e);
+            });
+    }
 
-    $http.get('/cakes/top5')
-        .then(function (res) {
-            self.top5 = res.data;
-        })
-        .catch(function (e) {
-            return Promise.reject(e);
-        });
 
     $scope.addToCart = function (productName ) {
         alert(productName);
