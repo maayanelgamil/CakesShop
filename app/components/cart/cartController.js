314411a32ebf28cdcf3fc036134633de85c1e53b
Maayan Elgamil <maayan.elgamil@gmail.com>
2017-07-02 16:58:06 +0300

    add "add to cart" to home view

--- app/components/cart/cartController.js
+++ app/components/cart/cartController.js
@@ -4,6 +4,7 @@
 app.controller('cartController', ['$scope', '$http','localStorageService', '$rootScope', 'cartService',
     function($scope, $http, localStorageService, $rootScope, cartService) {
         let self = this;
+        self.cart = localStorageService.get($rootScope.UserName);
 
         $scope.addToCart = function (productName ) {
             alert(productName);
