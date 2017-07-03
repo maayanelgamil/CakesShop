0b3127d50e54a0b673394f1d889a44dadaaa5047
Maayan Elgamil <maayan.elgamil@gmail.com>
2017-07-03 23:44:13 +0300

    add cakes controller

--- app/components/cakes/cakesController.js
+++ app/components/cakes/cakesController.js
@@ -3,11 +3,12 @@
 app.controller('cakesController', ['$scope', '$http','localStorageService','UserService', 'cakesService', '$rootScope',
     function($scope, $http, localStorageService, UserService, cakesService, $rootScope) {
         let self = this;
-        $http.get('/categories')
+        $http.get('/categories') // get categories
             .then(function (res) {
                 self.categories = res.data;
                 if(!$rootScope.guest && !$rootScope.allCakes) {
-                    cakesService.getRecommendedProducts().then(cakesService.allCakes);
+                    cakesService.getRecommendedProducts() // gets all the recommended cakes
+                        .then(cakesService.allCakes); // now all the cakes are save in $root.allCakes !
                 }
             })
             .catch(function (e) {
