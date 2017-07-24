648b2d8ac1dc80ffe6d101c0ae0b7a9c191db791
Maayan Elgamil <maayan.elgamil@gmail.com>
2017-07-24 21:53:31 +0300

    add function to cart

--- app/components/cart/cartController.js
+++ app/components/cart/cartController.js
@@ -1,13 +1,25 @@
 
 'use strict';
 //-------------------------------------------------------------------------------------------------------------------
-app.controller('cartController', ['$scope', '$http','localStorageService', '$rootScope', 'cartService',
-    function($scope, $http, localStorageService, $rootScope, cartService) {
+app.controller('cartController', ['$scope', '$http','localStorageService', '$rootScope',
+    function($scope, $http, localStorageService, $rootScope) {
         let self = this;
         self.cart = localStorageService.get($rootScope.UserName);
 
-        $scope.addToCart = function (productName ) {
-            alert(productName);
+        self.remove = function (cake) {
+            let index = self.cart.indexOf(cake);
+            self.cart.splice(index,1);
+        };
+
+        self.pay = function(){
+            var order =
+                { UserName: $rootScope.UserName,
+                    OrderDate: new Date(),
+                    ShipmentDate: "", // MISSING!!
+                    Dollar: "",
+                    TotalAmount: 0
+            };
+            $http.post('/addOrder', )
         };
     }]);
 
