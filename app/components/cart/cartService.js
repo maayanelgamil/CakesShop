cc6766c37c7e5bd8a6128e6d93d14f944a52d7ee
potashkeren <potashkeren@gmail.com>
2017-08-03 22:21:16 +0300

    login + register - change validation & error

--- app/components/cart/cartService.js
+++ app/components/cart/cartService.js
@@ -7,24 +7,29 @@ app.factory('cartService', ['$http', 'localStorageService', '$filter', '$rootSco
         service.selectedCake = null;
 
         service.addToCart = function (cake) {
-            let valueStored = localStorageService.get($rootScope.UserName);
-            if (!valueStored){ // first cake in the cart
-                cake.Amount = 1;
-                localStorageService.set($rootScope.UserName, [cake]);
-                alert('Cake was added successfully');
-
-            } else{
-                var lookup = {};
-                for (var i = 0, len = valueStored.length; i < len; i++) { //look for this cake using lookup table
-                    lookup[valueStored[i].CakeID] = valueStored[i];
-                }
-                var exist = lookup[cake.CakeID];
-                if(!exist){ // verify that the cake is not already in the cart
+            if ($rootScope.guest){
+                alert('If you want to shop please log in first!');
+            }
+            else{
+                let valueStored = localStorageService.get($rootScope.UserName);
+                if (!valueStored){ // first cake in the cart
                     cake.Amount = 1;
-                    valueStored.push(cake);
-                    localStorageService.set($rootScope.UserName,valueStored);
+                    localStorageService.set($rootScope.UserName, [cake]);
+                    alert('Cake was added successfully');
+
+                } else{
+                    var lookup = {};
+                    for (var i = 0, len = valueStored.length; i < len; i++) { //look for this cake using lookup table
+                        lookup[valueStored[i].CakeID] = valueStored[i];
+                    }
+                    var exist = lookup[cake.CakeID];
+                    if(!exist){ // verify that the cake is not already in the cart
+                        cake.Amount = 1;
+                        valueStored.push(cake);
+                        localStorageService.set($rootScope.UserName,valueStored);
+                    }
+                    alert('Cake was added successfully');
                 }
-                alert('Cake was added successfully');
             }
         };
 
